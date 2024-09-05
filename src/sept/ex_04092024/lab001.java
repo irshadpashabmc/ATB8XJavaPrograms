017a5c54b012d1e736a2b6da2b86478a52cbaa0b
irshadpashabmc <irshadpasha.bmc@gmail.com>
2024-09-05 16:09:52 +0530

    Initial commit done by irshad pasha Iam amending code

--- src/sept/ex_04092024/lab001.java
+++ src/sept/ex_04092024/lab001.java
@@ -1,4 +1,11 @@
 package sept.ex_04092024;
 
 public class lab001 {
+
+    public static void main(String[] args)  {
+
+        System.out.println("Hello Irshad welcome to the world of github lab001");
+
+    }
+
 }
--- src/sept/ex_04092024/lab002.java
+++ src/sept/ex_04092024/lab002.java
@@ -1,4 +1,10 @@
 package sept.ex_04092024;
 
 public class lab002 {
+
+    public static void main(String[] args)  {
+
+        System.out.println("Hello Irshad welcome to the world of github laboo2");
+
+    }
 }
