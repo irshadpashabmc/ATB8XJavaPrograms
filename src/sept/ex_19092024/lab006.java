1938b68cc25794f17e1bad3f9c1b69df5e943ad0
irshadpashabmc <irshadpasha.bmc@gmail.com>
2024-09-23 14:48:48 +0530

    commiting a program realted to arthematic operations

---
+++ src/sept/ex_19092024/lab006.java
@@ -0,0 +1,24 @@
+package sept.ex_19092024;
+
+public class lab006 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = a+b+c;
+        System.out.println(d+50);// 100
+        System.out.println(a+b-a+b);//40
+        System.out.println(a+d /a+c);//4.66
+        System.out.println(2.0F%5.0F);
+        
+
+
+
+
+
+
+
+    }
+}
